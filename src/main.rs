c42b4b4d22c0f62ed99844aff83bfcc103739499
Jiaxin Ying <112274822+helenyjx@users.noreply.github.com>
2023-02-10 03:37:41 +0000

    update

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+//A Simple Calculator Microservice that can only do addition, substracion, multiplication and division
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("This is a simple calculator microservice, please type: /add or subtract or divide or multiply/{a}/{b}")
+}
+
+//library add route using lib.rs
+#[get("/add/{a}/{b}")]
+async fn add(info: web::Path<(i32, i32)>) -> impl Responder {
+    let result = calc::add(info.0, info.1);
+    HttpResponse::Ok().body(result.to_string())
+}
+
+//library subtract route using lib.rs
+#[get("/subtract/{a}/{b}")]
+async fn subtract(info: web::Path<(i32, i32)>) -> impl Responder {
+    let result = calc::subtract(info.0, info.1);
+    HttpResponse::Ok().body(result.to_string())
+}
+
+//library multiply route using lib.rs
+#[get("/multiply/{a}/{b}")]
+async fn multiply(info: web::Path<(i32, i32)>) -> impl Responder {
+    let result = calc::multiply(info.0, info.1);
+    HttpResponse::Ok().body(result.to_string())
+}
+
+//library divide route using lib.rs
+#[get("/divide/{a}/{b}")]
+async fn divide(info: web::Path<(i32, i32)>) -> impl Responder {
+    let result = calc::divide(info.0, info.1);
+    HttpResponse::Ok().body(result.to_string())
+}
+
+//run it
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(add)
+            .service(subtract)
+            .service(multiply)
+            .service(divide)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
